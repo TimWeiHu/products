bf5e2000c00570eeee50eac8253d89a265830410
TimWeiHu <win94717@gmail.com>
2019-02-11 15:49:50 +0800

    check file

--- products.py
+++ products.py
@@ -1,12 +1,20 @@
+import os
+
 products = []
 
-with open('products.csv', 'r') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue
-       # name, price = 
-        products.append(line.strip().split(','))
+# 讀取檔案
+if os.path.isfile('products.csv'):
+    print('找到檔案')
+    with open('products.csv', 'r') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+           # name, price = 
+            products.append(line.strip().split(','))
+else:
+    print('找不到檔案')
 
+# 輸入新項目
 while True:
     name = input('請輸入商品名稱：')
     if name == 'q':
@@ -15,11 +23,11 @@ while True:
     price = int(price)
     p = [name, price]
     products.append(p)
-# print(products)
 print('---------------------')
+# 印出所有項目
 for p in products:
     print(p[0], '價格：', p[1])
-
+# 寫入檔案
 with open('products.csv', 'w') as f:
     f.write('商品,價格\n')
     for p in products:
